bbdcd2c30bd7e0f9aa3a8f748987e64e701d7395
DenisM2k1 <motocan.sorin@gmail.com>
2022-05-24 20:07:19 +0300

    variabile

--- euclid.cpp
+++ euclid.cpp
@@ -4,8 +4,11 @@ using namespace std;
 int main()
 {
     int a, b,k ;
+    cout << "Dati primul numar: ";
+    cin >> a;
 
-   
+    cout << "Dati al doilea numar: ";
+    cin >> b;
 
     return 0;
 }
